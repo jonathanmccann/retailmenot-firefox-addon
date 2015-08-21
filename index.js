5242045e3fde66ad690ac1d0e7752f42da0b3c21
Jonathan McCann <jonathan.mccann@liferay.com>
2015-08-21 15:20:58 -0700

    Create a toolbar button and context menu to search RetailMeNot for the current webpage URL to see if there are any coupons

---
+++ index.js
@@ -0,0 +1,31 @@
+// Set up necessary variables via "require"
+var { ActionButton } = require("sdk/ui/button/action");
+var contextMenu = require("sdk/context-menu");
+var self = require("sdk/self");
+var tabs = require("sdk/tabs");
+
+// Create toggle button for toolbar
+var button = ActionButton({
+	id: "search-retailmenot",
+	label: "Search RetailMeNot",
+	icon: {
+		"16": "./retailmenot-icon-16.png",
+		"32": "./retailmenot-icon-32.png",
+		"64": "./retailmenot-icon-64.png"
+	},
+	onClick: function(state) {
+		tabs.open("http://www.retailmenot.com/search.php?query=" + encodeURIComponent(tabs.activeTab.url));
+	}
+});
+
+// Create context menu item
+var menuItem = contextMenu.Item({
+	label: "Search RetailMeNot",
+	context: contextMenu.PageContext(),
+	contentScript: 'self.on("click", function () {' +
+				   '	self.postMessage();' +
+				   '});',
+	onMessage: function () {
+		tabs.open("http://www.retailmenot.com/search.php?query=" + encodeURIComponent(tabs.activeTab.url));
+	}
+});
